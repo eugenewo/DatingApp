5ba0740ef7164e251f8b3586428a046dd4eaceca
Eugene Wolfman <eugenewo@matrix.co.il>
2020-02-04 14:40:41 +0200

    .

--- datingApp-SPA/src/app/app.module.ts
+++ datingApp-SPA/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
